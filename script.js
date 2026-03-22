cc2490f2cf03e0dd29ee73ecd6bb603f4c32ea82
Likki Mahender Reddy <likkimahenderreddy123@gmail.com>
2026-03-22 20:24:16 +0530

    logs removed

--- script.js
+++ script.js
@@ -251,9 +251,6 @@ async function updateTaskInDatabase(taskId, taskData) {
             // Always update the modification timestamp
             cleanUpdateData.updatedAt = serverTimestamp();
 
-            console.log("Attempting Firestore update for task:", taskId);
-            console.log("Payload being sent:", cleanUpdateData);
-
             await updateDoc(taskRef, cleanUpdateData);
         }
     } catch (error) {
